9c3b827505fd99c82ca55e09d56ad59ab91e3743
riosaputra17 <riobadrun1721@gmail.com>
2023-07-04 19:57:29 +0700

    menambahkan fitur login

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'rioosaputraa17@gmail.com';
+    public string $fromName   = 'WebPrograming2';
     public string $recipients = '';
 
     /**
--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -1,8 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
         <a class="navbar-brand" href="#">MobilMantap.com</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
@@ -10,7 +9,8 @@
                 <a class="nav-item nav-link active" href="/">Home</a>
                 <a class="nav-item nav-link" href="about">About</a>
                 <a class="nav-item nav-link" href="contact">Contact</a>
-                <a class="nav-item nav-link" href="/komik">Car Listing</a>
+                <a class="nav-item nav-link" href="/komik">Komik</a>
+                <a class="nav-item nav-link" href="/komik">Login/Sign Up</a>
             </div>
         </div>
     </div>
